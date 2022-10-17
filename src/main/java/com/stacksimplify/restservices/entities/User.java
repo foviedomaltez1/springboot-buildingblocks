7d6fac7b61cb04fc0e37fca98b65d80f5a5ded1d
foviedomaltez1 <115769400+foviedomaltez1@users.noreply.github.com>
2022-10-17 11:51:12 -0500

    06-SpringBoot-JPA-OneToMany

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -1,15 +1,31 @@
 package com.stacksimplify.restservices.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-//Entity
-// and
-@Entity
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Entity
+    // and
+    @Entity
     @Table(name = "user")
     public class User {
 
@@ -25,16 +41,6 @@ import javax.validation.constraints.Size;
         @Column(name = "FIRST_NAME", length = 50, nullable = false)
         private String firstname;
 
-
-
-
-
-
-
-
-
-
-
         @Column(name = "LAST_NAME", length = 50, nullable = false)
         private String lastname;
 
@@ -47,83 +53,119 @@ import javax.validation.constraints.Size;
         @Column(name = "SSN", length = 50, nullable = false, unique = true)
         private String ssn;
 
+        @OneToMany(mappedBy="user")
+        private List<Order> orders;
+
         // No Argument Constructor
         public User() {
-        }
 
-        // Fields Constructor
-        public User(Long id, String username, String firstname, String lastname, String email, String role, String ssn) {
-            this.id = id;
-            this.username = username;
-            this.firstname = firstname;
-            this.lastname = lastname;
-            this.email = email;
-            this.role = role;
-            this.ssn = ssn;
-        }
 
-        // Getters and Setters
-        public Long getId() {
-            return id;
-        }
 
-        public void setId(Long id) {
-            this.id = id;
-        }
 
-        public String getUsername() {
-            return username;
-        }
 
-        public void setUsername(String username) {
-            this.username = username;
-        }
 
-        public String getFirstname() {
-            return firstname;
-        }
 
-        public void setFirstname(String firstname) {
-            this.firstname = firstname;
-        }
 
-        public String getLastname() {
-            return lastname;
-        }
 
-        public void setLastname(String lastname) {
-            this.lastname = lastname;
-        }
 
-        public String getEmail() {
-            return email;
-        }
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
 
-        public String getRole() {
-            return role;
-        }
+            }
 
-        public void setRole(String role) {
-            this.role = role;
-        }
+            // Fields Constructor
+		public User(Long id, String username, String firstname, String lastname, String email, String role, String ssn) {
+                this.id = id;
+                this.username = username;
+                this.firstname = firstname;
+                this.lastname = lastname;
+                this.email = email;
+                this.role = role;
+                this.ssn = ssn;
+            }
 
-        public String getSsn() {
-            return ssn;
-        }
+            // Getters and Setters
+            public Long getId() {
+                return id;
+            }
 
-        public void setSsn(String ssn) {
-            this.ssn = ssn;
-        }
+            public void setId(Long id) {
+                this.id = id;
+            }
 
-        // To String
-        @Override
-        public String toString() {
-            return "User [id=" + id + ", username=" + username + ", firstname=" + firstname + ", lastname=" + lastname
-                    + ", email=" + email + ", role=" + role + ", ssn=" + ssn + "]";
-        }
+            public String getUsername() {
+                return username;
+            }
 
-    }
+            public void setUsername(String username) {
+                this.username = username;
+            }
+
+            public String getFirstname() {
+                return firstname;
+            }
+
+            public void setFirstname(String firstname) {
+                this.firstname = firstname;
+            }
+
+            public String getLastname() {
+                return lastname;
+            }
+
+            public void setLastname(String lastname) {
+                this.lastname = lastname;
+            }
+
+            public String getEmail() {
+                return email;
+            }
+
+            public void setEmail(String email) {
+                this.email = email;
+            }
+
+            public String getRole() {
+                return role;
+            }
+
+            public void setRole(String role) {
+                this.role = role;
+            }
+
+            public String getSsn() {
+                return ssn;
+            }
+
+            public void setSsn(String ssn) {
+                this.ssn = ssn;
+            }
+
+
+
+
+            public List<Order> getOrders() {
+                return orders;
+            }
+
+            public void setOrders(List<Order> orders) {
+                this.orders = orders;
+            }
+
+            // To String
+            @Override
+            public String toString() {
+
+
+
+
+
+
+
+
+
+
+                return "User [id=" + id + ", username=" + username + ", firstname=" + firstname + ", lastname=" + lastname
+                        + ", email=" + email + ", role=" + role + ", ssn=" + ssn + "]";
+            }
+
+        }
