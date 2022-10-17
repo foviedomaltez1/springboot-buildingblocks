eb08eeb1aad31201fb7f9fc03b24a41a1225a366
foviedomaltez1 <115769400+foviedomaltez1@users.noreply.github.com>
2022-10-17 08:57:18 -0500

    05-Validations-GlobalExceptionHandler

--- src/main/java/com/stacksimplify/restservices/exceptions/UserNotFoundException.java
+++ src/main/java/com/stacksimplify/restservices/exceptions/UserNotFoundException.java
@@ -3,12 +3,12 @@ package com.stacksimplify.restservices.exceptions;
 public class UserNotFoundException  extends Exception {
 
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-
-
+	public UserNotFoundException(String message) {
+		super(message);
+		}
+	
+	
 
 }
