5103ee0dd03a0b2b57616996267ffeafe8fd1175
foviedomaltez1 <115769400+foviedomaltez1@users.noreply.github.com>
2022-10-17 15:04:46 -0500

    07-SpringBoot-HATEOAS

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -4,12 +4,6 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 
 
 
@@ -19,153 +13,127 @@ import javax.validation.constraints.Size;
 
 
 
+        import javax.persistence.GeneratedValue;
+        import javax.persistence.Id;
+        import javax.persistence.OneToMany;
+        import javax.persistence.Table;
+        import javax.validation.constraints.NotEmpty;
+        import javax.validation.constraints.Size;
 
+        import org.springframework.hateoas.ResourceSupport;
 
+//Entity
+// and
+public class User extends ResourceSupport {
 
 
-    //Entity
-    // and
-    @Entity
-    @Table(name = "user")
-    public class User {
+    private Long userid;
 
-        @Id
-        @GeneratedValue
-        private Long id;
 
-        @NotEmpty(message = "Username is Mandatory field. Please provide username")
-        @Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
-        private String username;
+    private String username;
 
-        @Size(min=2, message="FirstName should have atleast 2 characters")
-        @Column(name = "FIRST_NAME", length = 50, nullable = false)
-        private String firstname;
+    private String firstname;
 
-        @Column(name = "LAST_NAME", length = 50, nullable = false)
-        private String lastname;
+    private String lastname;
 
-        @Column(name = "EMAIL_ADDRESS", length = 50, nullable = false)
-        private String email;
+    private String email;
 
-        @Column(name = "ROLE", length = 50, nullable = false)
-        private String role;
+    private String role;
 
-        @Column(name = "SSN", length = 50, nullable = false, unique = true)
-        private String ssn;
+    private String ssn;
 
-        @OneToMany(mappedBy="user")
-        private List<Order> orders;
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
 
-        // No Argument Constructor
-        public User() {
+    // No Argument Constructor
+    public User() {
+    }
 
+    // Fields Constructor
+    public User(Long userid,
+                String email, String role, String ssn, List<Order> orders) {
+        super();
+        this.userid = userid;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.role = role;
+        this.ssn = ssn;
+        this.orders = orders;
+    }
+    // Getters and Setters
 
+    public Long getUserid() {
+        return userid;
+    }
 
+    public void setUserid(Long userid) {
+        this.userid = userid;
+    }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
+    public String getFirstname() {
+        return firstname;
+    }
 
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
+    public String getLastname() {
+        return lastname;
+    }
 
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-            }
+    public String getRole() {
+        return role;
+    }
 
-            // Fields Constructor
-		public User(Long id, String username, String firstname, String lastname, String email, String role, String ssn) {
-                this.id = id;
-                this.username = username;
-                this.firstname = firstname;
-                this.lastname = lastname;
-                this.email = email;
-                this.role = role;
-                this.ssn = ssn;
-            }
+    public void setRole(String role) {
+        this.role = role;
+    }
 
-            // Getters and Setters
-            public Long getId() {
-                return id;
-            }
+    public String getSsn() {
+        return ssn;
+    }
 
-            public void setId(Long id) {
-                this.id = id;
-            }
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
 
-            public String getUsername() {
-                return username;
-            }
+    public List<Order> getOrders() {
+        return orders;
+    }
 
-            public void setUsername(String username) {
-                this.username = username;
-            }
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 
-            public String getFirstname() {
-                return firstname;
-            }
+    // To String
 
-            public void setFirstname(String firstname) {
-                this.firstname = firstname;
-            }
 
-            public String getLastname() {
-                return lastname;
-            }
+    public String toString() {
+        return "User [userid=" + userid + ", username=" + username + ", firstname=" + firstname + ", lastname="
+                + lastname + ", email=" + email + ", role=" + role + ", ssn=" + ssn + ", orders=" + orders + "]";
+    }
 
-            public void setLastname(String lastname) {
-                this.lastname = lastname;
-            }
-
-            public String getEmail() {
-                return email;
-            }
-
-            public void setEmail(String email) {
-                this.email = email;
-            }
-
-            public String getRole() {
-                return role;
-            }
-
-            public void setRole(String role) {
-                this.role = role;
-            }
-
-            public String getSsn() {
-                return ssn;
-            }
-
-            public void setSsn(String ssn) {
-                this.ssn = ssn;
-            }
-
-
-
-
-            public List<Order> getOrders() {
-                return orders;
-            }
-
-            public void setOrders(List<Order> orders) {
-                this.orders = orders;
-            }
-
-            // To String
-            @Override
-            public String toString() {
-
-
-
-
-
-
-
-
-
-
-                return "User [id=" + id + ", username=" + username + ", firstname=" + firstname + ", lastname=" + lastname
-                        + ", email=" + email + ", role=" + role + ", ssn=" + ssn + "]";
-            }
-
-        }
+}
