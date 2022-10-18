9445520b632f9dc7262de95ec49c9cbece4e94ee
foviedomaltez1 <115769400+foviedomaltez1@users.noreply.github.com>
2022-10-17 21:43:26 -0500

    09-03-SpringBoot-Filtering-JsonView

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -1,7 +1,5 @@
 package com.stacksimplify.restservices.entities;
-
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,78 +9,67 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-
-
-
 import org.springframework.hateoas.EntityModel;
 
-
-
-
-import com.fasterxml.jackson.annotation.JsonFilter;
-
-
-
-
+import com.fasterxml.jackson.annotation.JsonView;
 
 //Entity
-
 // and
-
 @Entity
-
 @Table(name = "user")
-
 //@JsonIgnoreProperties({"firstname", "lastname"})  -- Static Filtering @JsonIgnore
-
-@JsonFilter(value = "userFilter")
-
+//@JsonFilter(value = "userFilter")  -- Used for MappingJacksonValue filtering section
 public class User extends EntityModel {
-
-
-
 
     @Id
     @GeneratedValue
+    @JsonView(Views.External.class)
     private Long userid;
 
     @NotEmpty(message = "Username is Mandatory field. Please provide username")
     @Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
+    @JsonView(Views.External.class)
     private String username;
 
     @Size(min = 2, message = "FirstName should have atleast 2 characters")
     @Column(name = "FIRST_NAME", length = 50, nullable = false)
+    @JsonView(Views.External.class)
     private String firstname;
 
     @Column(name = "LAST_NAME", length = 50, nullable = false)
+    @JsonView(Views.External.class)
     private String lastname;
 
     @Column(name = "EMAIL_ADDRESS", length = 50, nullable = false)
+    @JsonView(Views.External.class)
     private String email;
 
     @Column(name = "ROLE", length = 50, nullable = false)
-
+    @JsonView(Views.Internal.class)
     private String role;
 
-
-
-
     @Column(name = "SSN", length = 50, nullable = false, unique = true)
-
     //@JsonIgnore -- Static Filtering @JsonIgnore
-
+    @JsonView(Views.Internal.class)
     private String ssn;
 
-
-
-
     @OneToMany(mappedBy = "user")
+    @JsonView(Views.Internal.class)
     private List<Order> orders;
 
     // No Argument Constructor
+
+
+
+
+
+
+
+
+
+
     public User() {
     }
-
     // Fields Constructor
     public User(Long userid,
                 @NotEmpty(message = "Username is Mandatory field. Please provide username") String username,
@@ -99,77 +86,58 @@ public class User extends EntityModel {
         this.orders = orders;
     }
     // Getters and Setters
-
     public Long getUserid() {
         return userid;
     }
-
     public void setUserid(Long userid) {
         this.userid = userid;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getFirstname() {
         return firstname;
     }
-
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
-
     public String getLastname() {
         return lastname;
     }
-
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getRole() {
         return role;
     }
-
     public void setRole(String role) {
         this.role = role;
     }
-
     public String getSsn() {
         return ssn;
     }
-
     public void setSsn(String ssn) {
         this.ssn = ssn;
     }
-
     public List<Order> getOrders() {
         return orders;
     }
-
     public void setOrders(List<Order> orders) {
         this.orders = orders;
     }
-
     // To String
-
     @Override
     public String toString() {
         return "User [userid=" + userid + ", username=" + username + ", firstname=" + firstname + ", lastname="
                 + lastname + ", email=" + email + ", role=" + role + ", ssn=" + ssn + ", orders=" + orders + "]";
     }
-
 }
